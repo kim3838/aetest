9d776744e8298d8afb533e66aaa1bfc862ffe27f
Kim@PC <kimdeguzman20@yahoo.com>
2022-07-26 16:28:31 +0800

    _

--- services/dbconnection.php
+++ services/dbconnection.php
@@ -8,7 +8,7 @@ try {
     $username = "u624157578_aeptest_user";
     $password = "7|X69@wO";
     $database = "u624157578_aeptest";
-    $databaseHandler=new PDO("mysql:host=$hostname;dbname=$database","root","");
+    $databaseHandler=new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
     $databaseHandler->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_USE_BUFFERED_QUERY);
 } catch (PDOException $e) {
     echo $app->serverErrorResponse([$e->getMessage()]);
